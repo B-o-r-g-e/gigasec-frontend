b673827c9d2e6185917ecefe724fc2714866e7b2
b_o_r_g_e <babalolaoluwapelumi9@gmail.com>
2026-03-12 09:36:38 +0100

    Initial commit

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,12 +2,12 @@ import Navbar from "@/components/layout/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="">
+        <div className="relative">
             <div><Navbar /></div>
             {/*<div className="w-full flex-none md:w-64">*/}
             {/*    <SideNav />*/}
             {/*</div>*/}
-            <div id="marketing-scroll-container" className="grow md:overflow-y-auto">{children}</div>
+            <div id="marketing-scroll-container" className="grow">{children}</div>
         </div>
     );
 }
