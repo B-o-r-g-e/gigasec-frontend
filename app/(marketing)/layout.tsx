c04a81a411eb9429004901b9a8f27947981aa09b
b_o_r_g_e <babalolaoluwapelumi9@gmail.com>
2026-03-12 18:18:45 +0100

    created sidebar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,12 @@
-import Navbar from "@/components/layout/navbar";
+import Navbar, {MobileMenu} from "@/components/layout/navbar";
+import {Sidebar} from "lucide-react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative">
             <div><Navbar /></div>
+            <MobileMenu />
+            {/*<div><Sidebar /></div>*/}
             {/*<div className="w-full flex-none md:w-64">*/}
             {/*    <SideNav />*/}
             {/*</div>*/}
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -3,6 +3,110 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { dMSans } from "@/app/ui/fonts";
+import {Menu, X} from "lucide-react";
+
+export function MobileMenu() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 40);
+        };
+
+        handleScroll(); // run once on mount
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <nav
+            className={`md:hidden fixed left-0 right-0 top-0 z-50 border-b px-8 py-4 transition-all duration-300 ease-in-out ${
+                scrolled
+                    ? "border-[#0099cc]/20 backdrop-blur-md shadow-[0_10px_30px_rgba(0,20,51,0.18)]"
+                    : "border-transparent bg-transparent"
+            }`}
+            style={{ backgroundColor: scrolled ? BRAND.navy : 'transparent' }}
+        >
+            <div className="flex justify-between items-center">
+                <div className="inline-flex items-center rounded-lg bg-white/90 px-3 py-2 backdrop-blur-sm">
+                    <Link className="" href="/">
+                        <Image
+                            src="/logo.jpeg"
+                            width={96}
+                            height={1}
+                            alt="Gigasec Logo"
+                        />
+                    </Link>
+                </div>
+
+                <Sidebar />
+            </div>
+        </nav>
+    )
+}
+
+function Sidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isOpen]);
+
+    return (
+        <div className="md:hidden">
+            <Menu
+                color="white"
+                className="cursor-pointer"
+                onClick={() => {
+                    setIsOpen(prev => !prev)
+                    console.log("Clicked")
+                }}
+            />
+            {isOpen && (
+                <div className="fixed bg-white inset-0 z-40 /*bg-white/10 backdrop-blur-xl shadow-2xl*/">
+                    <div className="py-8 px-4 flex justify-end border-b border-b-gray-100">
+                        <X
+                            className="cursor-pointer"
+                            onClick={() => setIsOpen(false)}
+                        />
+                    </div>
+                    <div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                        <div>somethig </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
 
 const navlinks = [
     { text: "Services", url: "#" },
@@ -15,9 +119,6 @@ const navlinks = [
 const BRAND = {
     navy: "#0d3d3d",
     electric: "#339a99",
-    orange: "#FF6600",
-    white: "#FFFFFF",
-    offwhite: "#F5F7FA",
     charcoal: "#1A1A2E",
     gray: "#6B7280",
 };
@@ -41,7 +142,8 @@ export default function Navbar() {
 
     return (
         <nav
-            className={`fixed left-0 right-0 top-0 z-50 border-b px-8 py-4 transition-all duration-300 ease-in-out ${
+            id="hero"
+            className={`hidden md:block fixed left-0 right-0 top-0 z-50 border-b px-8 py-4 transition-all duration-300 ease-in-out ${
                 scrolled
                         ? "border-[#0099cc]/20 backdrop-blur-md shadow-[0_10px_30px_rgba(0,20,51,0.18)]"
                     : "border-transparent bg-transparent"
@@ -49,17 +151,18 @@ export default function Navbar() {
             style={{ backgroundColor: scrolled ? BRAND.navy : 'transparent' }}
         >
             <div className="mx-auto flex max-w-7xl items-center justify-between ">
-                <Link href="/">
-                    <Image
-                        src="/logo.jpeg"
-                        width={150}
-                        height={1000}
-                        alt="Gigasec Logo"
-                        className="bg-white"
-                    />
-                </Link>
+                <div className="inline-flex items-center rounded-lg bg-white/90 px-3 py-2 backdrop-blur-sm">
+                    <Link className="" href="/">
+                        <Image
+                            src="/logo.jpeg"
+                            width={96}
+                            height={1}
+                            alt="Gigasec Logo"
+                        />
+                    </Link>
+                </div>
 
-                <div className="flex items-center gap-10">
+                <div className="md:flex hidden items-center gap-10">
                     <div className={`${dMSans.className} flex gap-10 antialiased`}>
                         {navlinks.map((link) => (
                             <Link
@@ -82,13 +185,4 @@ export default function Navbar() {
             </div>
         </nav>
     );
-}
-
-
-export function SideBar() {
-    const [isOpen, setIsOpen] = useState(false)
-    return (
-        <div className="md:hidden fixed inset-0 bg-white">
-        </div>
-    )
 }
